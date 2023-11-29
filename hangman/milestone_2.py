4d897edfa04a777004035ef48c98ed32100529ee
Piotr Ogiela <piotrogiela@gmail.com>
2023-11-29 19:38:41 +0000

    Adds Milestone_2 Tasks 1-4

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,3 +1,19 @@
-word_list = ['banana', 'apple', 'pineaple', 'melon', 'grapefruit']
+from curses.ascii import isalpha
+import random
 
+# Task 1
+word_list = ['banana', 'apple', 'pineaple', 'melon', 'grapefruit']
 print(word_list)
+
+# Task 2
+word = random.choice(word_list)
+print(word)
+
+# Task 3
+guess = input('Enter a single letter: ')
+
+# Task 4
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
